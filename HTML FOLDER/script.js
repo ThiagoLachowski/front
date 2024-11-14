6c77afd25176a7f09c8c3ec1bfaed6b46c37fb3b
ThiagoLachowski <thiagopaes2100@gmail.com>
2024-11-13 23:40:39 -0300

    Add files via upload

---
+++ HTML FOLDER/script.js
@@ -0,0 +1,5 @@
+let prevButton = document.getElementById('prev');
+let nextButton = document.getElementById("next");
+let rontainer = document.querySelector('.container')
+let items = container.querySelectorAll('.list.item')
+
